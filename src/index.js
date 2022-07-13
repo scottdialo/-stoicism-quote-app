b122021fa6a303a34ab369d4d09c493ca2fcc359
Scott Dialo <scottdialo@Scotts-MacBook-Pro.local>
2022-07-12 21:55:23 -0700

    commit for deploy to heroku

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function currentTime() {
+  const element = (
+    <div className="container">
+      {/* <h1 className="title">Time App</h1> */}
+      <h2 className="time"> {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  root.render(element);
+}
+
+setInterval(currentTime, 1000);
+
+//create a function taht will display a quote from an api every 60hr or when user click on refresh button
+const apiList = [];
+//find api or create your own
+//make api call
+//save quote in an array
+//append result on page element
+//set interval to refresh new quote
