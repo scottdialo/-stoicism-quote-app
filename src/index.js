c9c4b27c5ec855b3fee79aecb5f3f84bafc1ff96
Scott Dialo <scottdialo@Scotts-MacBook-Pro.local>
2022-07-16 22:16:44 -0700

    added weather api

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// import { response } from "express";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -40,7 +41,7 @@ getapi(api_url);
 //Weather display api call
 const weatherApiUrl =
   "https://api.openweathermap.org/data/2.5/weather?lat=38&lon=-121&units=imperial&appid=0436d4bf7cd3cb70116b6a7979f72384";
-const weatherIcon = "openweathermap.org/img/wn/10d@2x.png";
+//const weatherIcon = "openweathermap.org/img/wn/";
 
 async function getWeatherApi(weatherApiUrl) {
   const response = await fetch(weatherApiUrl);
@@ -52,10 +53,26 @@ async function getWeatherApi(weatherApiUrl) {
 
   document.getElementById("weatherId").innerText =
     weatherData.weather[0].description;
-  document
-    .getElementById("weatherIcon")
-    .src(" http://openweathermap.org/img/wn/10d@2x.png");
+  const icon = weatherData.weather[0].icon;
+  console.log(icon);
+
+  // document.getElementById("weatherIcon").src =
+  //   "http://openweathermap.org/img/wn/" + icon + ".png";
 }
 getWeatherApi(weatherApiUrl);
 
 //Detect user location to display weather data automatically
+
+const userLocationUrl =
+  "https://api.geoapify.com/v1/ipinfo?apiKey=3c31759e1d5a422db7b5973d07a3ffe1";
+
+async function userGeoLocation(userLocationUrl) {
+  // saving the response
+  const response = await fetch(userLocationUrl);
+
+  // converting data into json and saving into a var
+  var data = await response.json();
+  console.log(data);
+}
+// Calling that async function
+userGeoLocation(userLocationUrl);
